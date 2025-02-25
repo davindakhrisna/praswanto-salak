925e38f0592a44d6366e68046ad0ee28acaf63c4
davindakhrisna <arpeggio.gns@gmail.com>
2025-02-25 21:03:03 +0700

    update

--- express-prisma-mongoose/index.js
+++ express-prisma-mongoose/index.js
@@ -1,9 +1,26 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+import { PrismaClient } from "@prisma/client";
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB Atlas'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const prisma = new PrismaClient();
+
+async function createUser() {
+  try {
+    console.log("Prisma client:", prisma); // Debugging: Cek apakah prisma didefinisikan
+    console.log("Prisma userEmail model:", prisma.UserEmail); // Debugging: Cek apakah prisma.userEmail didefinisikan
+
+    const newUser = await prisma.UserEmail.create({
+      data: {
+        name: "Dilli Pandhodho",
+        email: "dilli.pandhodho@example.com",
+        password: "hashedPassword123", // Pastikan ini adalah hash dari password asli
+        gambar: "https://example.com/avatar.jpg", // Opsional
+      },
+    });
+    console.log("User created:", newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+createUser();
